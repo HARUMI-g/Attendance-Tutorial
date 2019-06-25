e0fc2f901c1fd5652fa1b7bbe38e23b3b29f0018
lolamini <lola.mini76@gmail.com>
2019-06-25 19:49:31 +0000

    Finish add-basic-info

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
   root 'static_pages#top'
   get '/signup', to: 'users#new'
@@ -8,5 +7,10 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
 end
